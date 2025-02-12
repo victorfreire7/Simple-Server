8c7fe005fca1b577fff1f35a5201e816ee25591d
tinhobwf <163362075+tinhobwf@users.noreply.github.com>
2025-02-12 19:53:08 -0300

    Delete 4 directory

--- 4/README.md
+++
@@ -1,7 +0,0 @@
-npm i express
-
-npm i ejs
-
-npm start   ---->     nodemon
-
-npm run dev      -------> webpack
--- 4/src/controllers/homeController.js
+++
@@ -1,7 +0,0 @@
-exports.paginaInicial = (req, res) => {
-    res.render('index');
-}
-
-exports.trataPost = (req, res) =>{
-    res.send(`O que você enviou foi: ${req.body.nome}`)
-};
